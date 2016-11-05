d55f1926e5af428ec5e446fa67781814408b1c2e
Sam Rang <Sam Rang>
2016-11-05 14:16:51 -0500

    Application set up

--- src/main/java/org/srang/bracketpicker/ManagerApplication.java
+++ src/main/java/org/srang/bracketpicker/ManagerApplication.java
@@ -1,12 +1,14 @@
 package org.srang.bracketpicker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApplication.class, args);
+    }
 }
---
+++ src/main/java/org/srang/bracketpicker/controller/UserController.java
@@ -0,0 +1,10 @@
+package org.srang.bracketpicker.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by samuelrang on 11/5/2016.
+ */
+@Controller
+public class UserController {
+}
