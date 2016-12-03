c34e9caea70674580b702f750150d800f12818db
Sam Rang <srang2010@gmail.com>
2016-12-03 09:52:40 -0600

    rename some stuff

--- gulpfile.js
+++ gulpfile.js
@@ -10,24 +10,44 @@ var gulp = require('gulp'),
     del = require('del');
 var autoprefixer = new LessAutoprefixer({ browsers: ['last 2 versions'] });
 
-gulp.task('style', function() {
-    return gulp.src('src/main/resources/static/styles/**/*.{less,css}')
+gulp.task('setup', function() {
+    gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/select2/dist/js/select2.min.js',
+        'node_modules/tinycolor/tinycolor.js',
+        'node_modules/pick-a-color/build/1.2.3/js/pick-a-color-1.2.3.min.js',
+        'node_modules/summernote/dist/summernote.min.js',
+        'node_modules/datatables/media/js/jquery.dataTables.min.js'
+    ])
+        .pipe(gulp.dest('src/main/resources/static/dist/js'));
+
+    gulp.src([
+    ])
+        .pipe(gulp.dest('src/main/resources/static/dist/css'));
+
+    gulp.src([
+        'node_modules/bootstrap/less/**/*.less',
+        'node_modules/font-awesome/less/*.less'
+    ])
+        .pipe(gulp.dest('src/main/resources/less'))
+});
+
+gulp.task('style', ['setup'], function() {
+    return gulp.src('src/main/resources/static/less/**/*.{less,css}')
         .pipe(less({
             paths: [path.join(__dirname, 'less', 'includes')],
             plugins: [autoprefixer]
         }))
-        .pipe(gulp.dest('src/main/resources/static/dist'))
+        .pipe(gulp.dest('src/main/resources/static/dist/css'))
         .pipe(rename({ suffix: '.min' }))
         .pipe(css())
-        .pipe(gulp.dest('src/main/resources/static/dist'))
-        .pipe(notify({ message: 'Styles task complete' }));
+        .pipe(gulp.dest('src/main/resources/static/dist/css'));
 });
 gulp.task('clean', function() {
     return del(['src/main/resources/static/dist']);
 });
-// gulp.task('default', ['clean'], function() {
-//     gulp.start('styles', 'scripts', 'images');
-// });
+
 gulp.task('default', function() {
     gulp.start('clean');
 })
