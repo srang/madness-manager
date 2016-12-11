e17ed110d9063b80ee6d98263c9d0fb02cc5f080
Sam Rang <srang2010@gmail.com>
2016-12-11 11:38:03 -0600

    registration form

---
+++ src/main/java/org/srang/madness/manager/model/forms/RegisterForm.java
@@ -0,0 +1,29 @@
+package org.srang.madness.manager.model.forms;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+//import org.hibernate.validator.constraints.Length;
+
+/**
+ * Created by srang on 12/10/16.
+ */
+@Getter
+@Setter
+public class RegisterForm {
+    @NotNull(message = "Email is a required field")
+    @Email(message = "Please enter a valid email address")
+    String email;
+    @NotNull(message = "First name is a required field")
+    String firstName;
+    @NotNull(message = "Last name is a required field")
+    String lastName;
+    @NotNull(message = "Favorite Team is a required field")
+    @Length(min = 2, max = 44, message = "Please keep team name between 2 and 44 characters")
+    String favoriteTeam;
+
+}
