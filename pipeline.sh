0126933d49f30c52d52992bd05745623d434a6e2
Sam Rang <srang2010@gmail.com>
2020-05-05 12:09:27 -0400

    templates

--- pipeline.sh
+++ pipeline.sh
@@ -5,11 +5,13 @@ set -e
 
 oc get secret gh-pkg-secret &>/dev/null || oc apply -f ./.configuration/gh-secret.yaml
 
-#oc cancel-build bc/madness-manager
+oc cancel-build bc/madness-manager
 #helm upgrade --dry-run --install  madness-manager ./src/main/k8s/madness-manager -n srang-quarkus
 
 helm upgrade --install  madness-manager ./src/main/k8s/madness-manager -n srang-quarkus
 
-#oc start-build bc/madness-manager
-#
-#oc logs -f bc/madness-manager
+oc start-build bc/madness-manager
+
+oc logs -f bc/madness-manager
+
+oc rollout restart deployment/madness-manager
