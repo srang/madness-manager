bdb5fe731793c88bb037db54daa01dd1641b7f52
Sam Rang <srang2010@gmail.com>
2020-05-13 13:52:12 -0400

    react

---
+++ bracket-ui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div>
+      <Button variant="contained" color="primary">
+        Hello
+      </Button>
+    </div>
+  );
+}
+
+export default App;
